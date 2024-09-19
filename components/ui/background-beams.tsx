cd8c3cbd94fbaad0039812354b393fadeca86a81
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-09-19 18:38:48 +0200

    removed beams from background

--- components/ui/background-beams.tsx
+++ components/ui/background-beams.tsx
@@ -71,51 +71,17 @@ export const BackgroundBeams = React.memo(({className}: {className?: string}) =>
           strokeOpacity="0.05"
           strokeWidth="0.5"></path>
 
-        {paths.map((path, index) => (
-          <motion.path key={`path-` + index} d={path} stroke={`url(#linearGradient-${index})`} strokeOpacity="0.4" strokeWidth="0.5"></motion.path>
-        ))}
-        <defs>
-          {paths.map((path, index) => (
-            <motion.linearGradient
-              id={`linearGradient-${index}`}
-              key={`gradient-${index}`}
-              initial={{
-                x1: "0%",
-                x2: "0%",
-                y1: "0%",
-                y2: "0%",
-              }}
-              animate={{
-                x1: ["0%", "100%"],
-                x2: ["0%", "95%"],
-                y1: ["0%", "100%"],
-                y2: ["0%", `${93 + Math.random() * 8}%`],
-              }}
-              transition={{
-                duration: Math.random() * 10 + 10,
-                ease: "easeInOut",
-                repeat: Infinity,
-                delay: Math.random() * 10,
-              }}>
-              <stop stopColor="#18CCFC" stopOpacity="0"></stop>
-              <stop stopColor="#18CCFC"></stop>
-              <stop offset="32.5%" stopColor="#6344F5"></stop>
-              <stop offset="100%" stopColor="#AE48FF" stopOpacity="0"></stop>
-            </motion.linearGradient>
-          ))}
-
-          <radialGradient
-            id="paint0_radial_242_278"
-            cx="0"
-            cy="0"
-            r="1"
-            gradientUnits="userSpaceOnUse"
-            gradientTransform="translate(352 34) rotate(90) scale(555 1560.62)">
-            <stop offset="0.0666667" stopColor="var(--neutral-300)"></stop>
-            <stop offset="0.243243" stopColor="var(--neutral-300)"></stop>
-            <stop offset="0.43594" stopColor="white" stopOpacity="0"></stop>
-          </radialGradient>
-        </defs>
+        <radialGradient
+          id="paint0_radial_242_278"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(352 34) rotate(90) scale(555 1560.62)">
+          <stop offset="0.0666667" stopColor="var(--neutral-300)"></stop>
+          <stop offset="0.243243" stopColor="var(--neutral-300)"></stop>
+          <stop offset="0.43594" stopColor="white" stopOpacity="0"></stop>
+        </radialGradient>
       </svg>
     </div>
   );
