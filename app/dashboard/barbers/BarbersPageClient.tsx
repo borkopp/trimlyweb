da9b00dbaa0a428639920b6409ef256df0c0728d
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-10-11 11:10:03 +0200

    Refactor button variant in BarbersPageClient

--- app/dashboard/barbers/BarbersPageClient.tsx
+++ app/dashboard/barbers/BarbersPageClient.tsx
@@ -250,7 +250,7 @@ export default function BarbersPageClient({
         </div>
         <Dialog open={isAddBarberDialogOpen} onOpenChange={setIsAddBarberDialogOpen}>
           <DialogTrigger asChild>
-            <Button>
+            <Button variant={"outline"}>
               <Plus className="mr-2 h-4 w-4" />
               Add New Barber
             </Button>
