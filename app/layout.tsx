cb90ca0d2db98a4941b1ef28389053de9205dc5f
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-10-28 12:00:50 +0100

    Refactor layout to include Vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,9 +44,9 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
           <Toaster />
+          <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
-        <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   );
